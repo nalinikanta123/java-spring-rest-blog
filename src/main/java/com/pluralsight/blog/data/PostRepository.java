e46be89a7d17dfe207f69743a2f159d9e3f93359
nalinikanta123 <nalinikantam@gmail.com>
2020-09-10 09:43:40 -0400

    Fixed @RestResource

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -3,10 +3,12 @@ package com.pluralsight.blog.data;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.pluralsight.blog.model.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+	@RestResource(rel="contains-title", path="containsTitle")
 	public List<Post> findByTitleContaining(String title);
 }
